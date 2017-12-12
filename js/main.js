9cfc58675bf867d21b4ee25792193cd3a9d4e194
jibonbiswas007 <33145139+jibonbiswas007@users.noreply.github.com>
2017-12-12 21:46:25 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,14 @@
+(function($) {
+    "use strict";
+    
+    jQuery(document).ready(function($){
+    	$(".header-area").sticky();
+        
+    });
+    
+    jQuery(window).load(function(){
+        
+    });
+    
+    
+}(jQuery));
